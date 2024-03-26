c5092060e2752e707350635c8d0e19d79cbe022f
Chris <rolletchris1@gmail.com>
2024-03-26 15:03:41 -0700

    initiate database

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,2 +1,63 @@
-// TO-DO: build the database connection using PostgresSQL
-package database
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/joho/godotenv"
+)
+
+func getEnvVar(key string) string {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	return os.Getenv(key)
+}
+
+func main() {
+	databaseUrl := getEnvVar("DATABASE_URL")
+	fmt.Printf("DATABASE_URL: %s\n", databaseUrl)
+	if len(databaseUrl) == 0 {
+		fmt.Fprintf(os.Stderr, "DATABASE_URL environment variable not set\n")
+		os.Exit(1)
+	}
+	// open database
+	dbPool, err := pgxpool.Connect(context.Background(), databaseUrl)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Connected!")
+
+	// test for create table and insert data
+	_, err = dbPool.Exec(context.Background(), "create table if not exists users(id serial primary key, username text, email text, password text, created_at timestamp, updated_at timestamp)")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating table: %v\n", err)
+		os.Exit(1)
+	}
+
+	_, err = dbPool.Exec(context.Background(), "insert into users(username, email, password, created_at, updated_at) values($1, $2, $3, $4, $5)", "Chris", "test@email.com", "1234", "2021-07-01", "2021-07-01")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error inserting data: %v\n", err)
+		os.Exit(1)
+	}
+
+	// query database test
+	var name string
+	err = dbPool.QueryRow(context.Background(), "select username from users").Scan(&name)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(name)
+
+	// close database
+	defer dbPool.Close()
+}
