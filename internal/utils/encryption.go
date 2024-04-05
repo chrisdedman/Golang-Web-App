61f1c9bb9877164d8931b83b8612dcd41f7d890b
Chris <rolletchris1@gmail.com>
2024-04-05 16:01:55 -0700

    refactor encryption package

---
+++ internal/utils/encryption.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"github.com/golang-web-app/config/models"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// HashPassword hashes the user's password using bcrypt.
+func HashPassword(user *models.User) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+
+	if err != nil {
+		return err
+	}
+	user.Password = string(hashedPassword)
+	return nil
+}
+
+// VerifyPassword compares a password with a hashed password.
+func VerifyPassword(password, hashedPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
