532731495fe8a9b6b032d4334ddf81249e4ee9de
Chris <rolletchris1@gmail.com>
2024-04-08 20:05:25 -0700

    Added delete account template

--- config/server/server.go
+++ config/server/server.go
@@ -47,6 +47,7 @@ func main() {
 	files := []string{
 		"views/user/authentication/login.html", "views/user/authentication/signup.html",
 		"views/user/authentication/logout.html", "views/user/account/update.html",
+		"views/user/account/delete.html",
 		"views/layout/footer.html", "views/layout/header.html",
 		"views/app/dashboard.html", "views/app/index.html", "views/errors.html",
 	}
