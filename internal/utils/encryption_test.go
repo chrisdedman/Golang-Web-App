8b6ae08df6b110822cf21d98ca3db82e75208867
Chris <rolletchris1@gmail.com>
2024-04-13 13:42:01 -0700

    refactor encryption test + added coverage

--- internal/utils/encryption_test.go
+++ internal/utils/encryption_test.go
@@ -9,28 +9,40 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-func TestHashPassword(t *testing.T) {
-	user := &models.User{
-		Password: "password123",
-	}
-
-	err := utils.HashPassword(user)
-	assert.NoError(t, err)
-
-	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte("password123"))
-	assert.NoError(t, err)
-
-	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte("password1234"))
-	assert.Error(t, err)
+var user = &models.User{
+	Username: "dev",
+	Password: "password123",
+	Email:    "dev@example.com",
 }
 
-func TestVerifyPassword(t *testing.T) {
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte("password123"), bcrypt.DefaultCost)
+/*
+ * Test cases for the HashPassword function
+ */
+func TestHashPasswordNoError(t *testing.T) {
+	err := utils.HashPassword(user)
+	assert.NoError(t, err)
+}
+
+func TestHashPasswordNil(t *testing.T) {
+	err := utils.HashPassword(user)
+	assert.Nil(t, err)
+}
+
+/*
+ * Test cases for the VerifyPassword function
+ */
+func TestVerifyPasswordNoError(t *testing.T) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
 	assert.NoError(t, err)
 
-	err = utils.VerifyPassword("password123", string(hashedPassword))
+	err = utils.VerifyPassword(user.Password, string(hashedPassword))
+	assert.NoError(t, err)
+}
+
+func TestVerifyPasswordError(t *testing.T) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte("password1"), bcrypt.DefaultCost)
 	assert.NoError(t, err)
 
-	err = utils.VerifyPassword("password1234", string(hashedPassword))
+	err = utils.VerifyPassword(user.Password, string(hashedPassword))
 	assert.Error(t, err)
 }
