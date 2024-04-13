ab62d0f80dbb7a9b87484f111c33e98a7409ffd3
Chris <rolletchris1@gmail.com>
2024-04-13 16:04:49 -0700

    refactor main to be the entry point of the app

--- main.go
+++ main.go
@@ -1,8 +1,39 @@
 package main
 
-import "github.com/golang-web-app/config/server"
+import (
+	"log"
+	"os"
 
-// main is the entry point for the application
+	"github.com/golang-web-app/config/database"
+	"github.com/golang-web-app/config/server"
+	"github.com/golang-web-app/internal/models"
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	server.Server()
+	err := godotenv.Load("./.env")
+	if err != nil {
+		log.Fatalf("Failed to load .env file: %v", err)
+	}
+
+	host := os.Getenv("HOST_ADDR")
+	if len(host) == 0 {
+		host = ":8080"
+	}
+
+	config := models.DatabaseConfig{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   os.Getenv("DB_NAME"),
+		SSLMode:  os.Getenv("DB_SSLMODE"),
+	}
+
+	db, err := database.InitDB(config)
+	if err != nil {
+		log.Fatalf("Failed to initialize database: %v", err)
+	}
+
+	server.Server(host, db)
 }
