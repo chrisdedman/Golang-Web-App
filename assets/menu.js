e3d59800d33fc418558d18863c2c8f26b7a96101
Chris <rolletchris1@gmail.com>
2024-04-12 20:06:16 -0700

    Added JS Account settings

--- assets/menu.js
+++ assets/menu.js
@@ -1,3 +1,22 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function settings() {
+  document.getElementById("setting").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
 /* 
   setmenu() function is used to set menu items dynamically
   the user to the selected menu option from the frontend.
