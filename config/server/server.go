54366a10a18aa18d5bafeb8020b2a0cb1a59f124
Chris <rolletchris1@gmail.com>
2024-04-13 16:02:49 -0700

    refactor server handler

--- config/server/server.go
+++ config/server/server.go
@@ -1,56 +1,27 @@
 package server
 
 import (
-	"log"
-	"os"
-
 	"github.com/gin-gonic/gin"
-	"github.com/golang-web-app/config/database"
 	"github.com/golang-web-app/internal/routes"
-	"github.com/joho/godotenv"
+	"gorm.io/gorm"
 )
 
-func Server() {
+func Server(host string, db *gorm.DB) {
 	router := gin.Default()
-
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	config := database.Config{
-		Host:     os.Getenv("DB_HOST"),
-		Port:     os.Getenv("DB_PORT"),
-		User:     os.Getenv("DB_USER"),
-		Password: os.Getenv("DB_PASSWORD"),
-		DBName:   os.Getenv("DB_NAME"),
-		SSLMode:  os.Getenv("DB_SSLMODE"),
-	}
-
-	host := os.Getenv("HOST_ADDR")
-	if len(host) == 0 {
-		host = ":8080"
-	}
-
 	router.ForwardedByClientIP = true
 	router.SetTrustedProxies([]string{"localhost"})
-
 	router.Static("assets", "./assets")
+
+	routes.AuthRoutes(router, db)
 
 	files := []string{
 		"views/user/authentication/login.html", "views/user/authentication/signup.html",
-		"views/user/authentication/logout.html", "views/user/account/update.html",
-		"views/user/account/delete.html",
+		"views/user/authentication/logout.html",
+		"views/user/account/update.html", "views/user/account/delete.html",
 		"views/layout/footer.html", "views/layout/header.html",
 		"views/app/dashboard.html", "views/app/index.html", "views/errors.html",
 	}
+
 	router.LoadHTMLFiles(files...)
-
-	db, err := database.InitDB(config)
-	if err != nil {
-		log.Fatalf("Failed to initialize database: %v", err)
-	}
-
-	routes.AuthRoutes(router, db)
 	router.Run(host)
 }
